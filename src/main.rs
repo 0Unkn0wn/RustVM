e43b0ce80d3e9a31a731f4d4fc86f0eb59947409
0Unkn0wn <mihnea.bastea@gmail.com>
2023-01-19 22:14:50 +0100

    added a todo list with the ideas I came up with during cooking

--- src/main.rs
+++ src/main.rs
@@ -13,3 +13,11 @@ fn main() {
     let instructions = VM::start_vm(file_path);
     VM::execute(instructions);
 }
+
+// TODO
+// - [ ] deci sa fie data section asa
+// are un numar langa care arata cate elemente o sa fie iniiatilizate
+// si efectiv da pop la ele si dupa le baga in stack care o sa fie un vector de field probabil declarat in vm
+// cand sunt sunate variabilele alea doar se iau din stack si cam atat
+// - [ ] pentru for fac asa copiez instructiunea aia de 3 ori si doar o execut de 3 ori
+// - [ ] ar fi bun daca as avea si un read ceva
