9f894dd03d1f8360e14f5aa38a0e0c5b6c0d46ec
0Unkn0wn <mihnea.bastea@gmail.com>
2023-01-26 02:56:13 +0100

    removed some items from the todo list in the main file would be nice if I manage to add a read function idk how it's going to work

--- src/main.rs
+++ src/main.rs
@@ -17,9 +17,4 @@ fn main() {
 }
 
 // TODO
-// - [ ] deci sa fie data section asa
-// are un numar langa care arata cate elemente o sa fie iniiatilizate
-// si efectiv da pop la ele si dupa le baga in stack care o sa fie un vector de field probabil declarat in vm
-// cand sunt sunate variabilele alea doar se iau din stack si cam atat
-// - [ ] pentru for fac asa copiez instructiunea aia de 3 ori si doar o execut de 3 ori
 // - [ ] ar fi bun daca as avea si un read ceva
