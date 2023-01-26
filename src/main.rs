1a2211f65ef1c7c4d03402e512778a02be67956c
0Unkn0wn <mihnea.bastea@gmail.com>
2023-01-26 13:50:40 +0100

    moved the txt outside the src folder and renamed it to assembly.txt

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ use crate::vm::VM;
 
 fn main() {
 
-    let file_path = "src/test.txt";
+    let file_path = "assembly.txt";
     let mut vm = VM::new();
     vm.execute(file_path);
     vm.print_stack();
